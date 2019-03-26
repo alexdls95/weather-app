b5fa880bfab51199312ff0ac93cc4e24798214e2
Alex de los Santos <adelossantos@topsystemscorp.com>
2019-03-26 17:59:31 -0300

    wip adding new action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,24 @@
+import { api_key, url_base_forecast } from './../constants/api_url'
+import transformForecast from './../services/transformForecast'
+
 export const SET_CITY = 'SET_CITY'
 
 export const setCity = payload => ({
   type: SET_CITY,
   payload,
 })
+
+export const fetchForecast = payload => {
+  return dispatch => {
+    const url_forecast = `${url_base_forecast}?q=${city}&appid=${api_key}`
+
+    fetch(url_forecast).then(
+      data => (data.json())
+    ).then(
+      weather_data => {
+        const forecastData = transformForecast(weather_data)
+        console.log(forecastData)
+      }
+    )
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import { city } from './../reducers/city'
+
 
 const initialState = {
   city: 'Montevideo,uy'
 }
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || DynamicsCompressorNode
 
 export const store = createStore (
   city,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  composeEnhancer(applyMiddleware(thunk))
+) 
